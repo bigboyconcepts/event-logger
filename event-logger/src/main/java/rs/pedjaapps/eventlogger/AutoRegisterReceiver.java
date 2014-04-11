8a7d79bacf001f848dd45d095cbbffeb102f773c
pedja <predragcokulov@gmail.com>
2014-04-11 16:20:42 +0200

    Initial Commit

---
+++ event-logger/src/main/java/rs/pedjaapps/eventlogger/AutoRegisterReceiver.java
@@ -0,0 +1,16 @@
+package rs.pedjaapps.eventlogger;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by pedja on 11.4.14..
+ */
+public class AutoRegisterReceiver extends AbsEventReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+    }
+}
